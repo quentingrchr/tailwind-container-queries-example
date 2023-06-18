c66d30ad17679f0f5aac7012c712b67ae367d5e5
quentingrchr <q.grancher@gmail.com>
2023-06-18 15:58:01 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,22 @@
+const articles = [
+    {
+        title: "A good article on CSS",
+        category: "CSS",
+        imageUrl: "https://images.unsplash.com/photo-1682686580186-b55d2a91053c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    title: "A good article on HTML",
+    category: "HTML",
+    imageUrl: "https://images.unsplash.com/photo-1611063482599-5b1b4b0e2b0f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHRtbCUyMGJhY2tncm91bmQlMjB3b3JsZHxlbnwwfHwwfHw%3D&ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    title: "A good article on JavaScript",
+    category: "JS",
+    imageUrl: "https://images.unsplash.com/photo-1611063482599-5b1b4b0e2b0f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHRtbCUyMGJhY2tncm91bmQlMjB3b3JsZHxlbnwwfHwwfHw%3D&ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=60"
+  },
+
+    
+];
+
+
+export default articles
